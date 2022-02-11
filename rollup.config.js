0e39397df11d4951c941cb2f631691dd35336599
Leonardo <leopiccionia@gmail.com>
2022-02-11 12:03:41 -0300

    Use ESBuild to transpile code.
    I want to use ESBuild for minification in the future, but their Rollup loaders
    does not handle UTF-8 characters very well.

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,37 @@
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import esbuild from 'rollup-plugin-esbuild'
 import { terser } from 'rollup-plugin-terser'
 
 const entries = ['index', 'ssr']
 
 const plugins = [
-    nodeResolve({ browser: true, preferBuiltins: true }),
-    commonjs(),
-    json(),
-    terser(),
+  nodeResolve({ browser: true, preferBuiltins: true }),
+  commonjs(),
+  json(),
+  esbuild({ target: 'es6' }),
+  terser(),
 ]
 
 const config = entries.map((slug) => {
-    return {
-        input: `src/${slug}.js`,
-        output: [
-            {
-                exports: 'named',
-                file: `dist/${slug}.umd.js`,
-                format: 'umd',
-                name: 'VueSanitizeDirective',
-            },
-            {
-                exports: 'named',
-                file: `dist/${slug}.esm.js`,
-                format: 'es',
-            },
-        ],
-        plugins,
-    }
+  return {
+    input: `src/${slug}.js`,
+    output: [
+      {
+        exports: 'named',
+        file: `dist/${slug}.umd.js`,
+        format: 'umd',
+        name: 'VueSanitizeDirective',
+      },
+      {
+        exports: 'named',
+        file: `dist/${slug}.esm.js`,
+        format: 'es',
+      },
+    ],
+    plugins,
+  }
 })
 
 export default config
