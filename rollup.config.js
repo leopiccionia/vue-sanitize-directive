cb459e3577756abd5835502dbec03debe86b8f92
Leonardo <leopiccionia@gmail.com>
2022-02-10 21:32:23 -0300

    Use Rollup for bundling.

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import nodeShims from 'rollup-plugin-polyfill-node'
+import { terser } from 'rollup-plugin-terser'
+
+const entries = ['index', 'ssr']
+
+const plugins = [
+    nodeShims(),
+    nodeResolve(),
+    json(),
+    commonjs(),
+    terser(),
+]
+
+const config = entries.map((slug) => {
+    return {
+        input: `src/${slug}.js`,
+        output: [
+            {
+                file: `dist/${slug}.umd.js`,
+                format: 'umd',
+                name: 'VueSanitizeDirective',
+            },
+            {
+                file: `dist/${slug}.esm.js`,
+                format: 'es',
+            },
+        ],
+        plugins,
+    }
+})
+
+export default config
