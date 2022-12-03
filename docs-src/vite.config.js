2ca2a4262c40b098f1a7c827c56937e366fdd9c9
Leonardo <leopiccionia@gmail.com>
2022-12-03 12:35:05 -0300

    Improve docs build

--- docs-src/vite.config.js
+++ docs-src/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  build: {
+    outDir: '../docs',
+  },
   base: '/vue-sanitize-directive/',
   plugins: [vue()],
   server: {
